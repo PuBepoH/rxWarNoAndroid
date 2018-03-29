d518dc37ab7de8f3c95c55c5d6976a98616db015
RagingPython <kaf32dmitrii@gmail.com>
2018-03-29 19:52:58 +0300

    just test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import io.reactivex.Observable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-
+        boolean exit=false;
+        Observable<String> q= null;
+        try {
+            q = Observable.just(
+                (new BufferedReader(new InputStreamReader(System.in))).readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Observable.
+        q=q.concatWith(q);
+        q=q.concatWith(q);
+        q.subscribe(System.out::println);
     }
 
 }
--- src/main/java/view/TestView.java
+++ src/main/java/view/TestView.java
@@ -1,28 +1,61 @@
 package view;
 
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.subjects.PublishSubject;
 import presenter.PresenterFacade;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TestView implements ViewFacade{
-    
+    private CompositeDisposable externalDiposables = new CompositeDisposable();
+    private CompositeDisposable internalDisposables = new CompositeDisposable();
+    private PublishSubject<String> menuActions;
+    private PublishSubject<MainAction> mainAction;
+
+    private Observable<String> inputFlow;
+
+    Boolean exit=false;
+
+    public void TestView(){
+
+        Observable<String> inputFlow = Observable.create(s->{
+
+            s.onNext("dsfgdfg");
+            s.onComplete();
+        });
+    }
 
     @Override
     public void initialize(PresenterFacade presenterFacade) {
-
+        externalDiposables.dispose();
+        externalDiposables.clear();
+        externalDiposables.add(presenterFacade.getFragmentControlState().subscribe(System.out::println));
     }
 
     @Override
     public PublishSubject<String> getMenuActions() {
-        return null;
+        return menuActions;
     }
 
     @Override
     public PublishSubject<MainAction> getMainActions() {
-        return null;
+        return mainAction;
     }
 
     @Override
     public PublishSubject<NewAction> getNewActions() {
         return null;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        externalDiposables.dispose();
+        externalDiposables.clear();
+        internalDisposables.dispose();
+        internalDisposables.clear();
+        super.finalize();
     }
 }
